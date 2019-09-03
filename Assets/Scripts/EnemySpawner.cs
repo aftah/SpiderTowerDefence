7bee75b50dbee5428b21a56e537a9499f4760342
aftah <abdelfetah.hamra@skynet.be>
2019-09-04 00:12:04 +0200

    Path Finding

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    [SerializeField] private GameObject enemy;
+    [SerializeField] float waveSpawnTime=5;
+    [SerializeField] private int enemyCount;
+    [SerializeField] private float timeToWait;
+    [SerializeField] private bool stopSpawning = false;
+
+
+    private void Start()
+    {
+        if(!stopSpawning)
+           InvokeRepeating("WaveSpawn",waveSpawnTime, waveSpawnTime);   
+    }
+
+    IEnumerator EnemyDrop()
+    {
+       
+        
+        for (int i = 0; i < enemyCount; i++)
+        {
+            Instantiate(enemy, transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(timeToWait); 
+        }
+    }
+
+    void WaveSpawn()
+    {
+        StartCoroutine(EnemyDrop());
+    }
+}
---
+++ Assets/Scripts/MoveEnemyToTarget.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveEnemyToTarget : MonoBehaviour
+{
+    [SerializeField] private List<Transform> listGatePosition;
+
+    private Transform gate;
+    private NavMeshAgent agent;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Start()
+    {
+        gate = listGatePosition[Random.Range(0, listGatePosition.Count)];
+    }
+    // Update is called once per frame
+    void Update()
+    {
+       
+        agent.destination = gate.transform.position;
+    }
+}
