0b637ab1ebafb8e2c101b64042eeba88cd46cf08
aftah <abdelfetah.hamra@skynet.be>
2019-09-03 17:28:07 +0200

    test spider

---
+++ Assets/Scripts/AgentDirection.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentDirection : MonoBehaviour
+{
+    [SerializeField] private Transform TargetPosition;
+
+    private NavMeshAgent agent;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(TargetPosition.position);
+    }
+
+  
+
+}
